d8f417ede35353d17924dbd38d0fe0c89fc39959
chackero <chaker.laribi@esprit.tn>
2014-11-09 22:27:54 +0100

    Adding template integration ( _FalconLayout.cshtml)

--- Falcon.Web/App_Start/BundleConfig.cs
+++ Falcon.Web/App_Start/BundleConfig.cs
@@ -22,9 +22,15 @@ namespace Falcon.Web
                       "~/Scripts/bootstrap.js",
                       "~/Scripts/respond.js"));
 
+            bundles.Add(new ScriptBundle("~/bundles/falcon").Include(
+                      "~/Content/js/*.js"));
+
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+
+            bundles.Add(new StyleBundle("~/Content/falcon/css").Include(
+                      "~/Content/css/*.css"));
 
             // Set EnableOptimizations to false for debugging. For more information,
             // visit http://go.microsoft.com/fwlink/?LinkId=301862
